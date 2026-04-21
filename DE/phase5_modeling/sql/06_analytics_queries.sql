fe5798f7c628558408b78481aa98732fffe7c8ff
KARTHEEKPUTTU <kartheekputtuffc@gmail.com>
2026-04-21 00:28:02 -0500

    Update 06_analytics_queries.sql

--- DE/phase5_modeling/sql/06_analytics_queries.sql
+++ DE/phase5_modeling/sql/06_analytics_queries.sql
@@ -8,7 +8,7 @@ SELECT d.location_name as location_name,f.ts::date as date,sum(f.precipitation) 
 FROM fact_weather_hourly f LEFT JOIN dim_location d ON f.location_id=d.location_id 
 GROUP BY d.location_name,f.ts::date ORDER BY date desc;
 
--- Top 5 windiest hours per city
+-- Top 5 windiest hours per city using ranked
 WITH ranked AS(
 SELECT d.location_name as location_name,f.ts  as ts,f.windspeed_10m AS windspeed_10m,ROW_NUMBER() OVER (PARTITION BY d.location_name 
 order by f.windspeed_10m DESC)  as rn
