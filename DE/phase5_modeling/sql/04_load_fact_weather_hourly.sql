0bdbf75f2ffc5444307b5510d8f6964dcd175886
KARTHEEKPUTTU <kartheekputtuffc@gmail.com>
2026-04-21 00:29:10 -0500

    Update 04_load_fact_weather_hourly.sql

--- DE/phase5_modeling/sql/04_load_fact_weather_hourly.sql
+++ DE/phase5_modeling/sql/04_load_fact_weather_hourly.sql
@@ -1,5 +1,6 @@
 INSERT INTO fact_weather_hourly(location_id,ts,temperature_2m,precipitation,windspeed_10m,date_key,run_id) 
-SELECT s.location_id,s.ts,s.temperature_2m,s.precipitation,s.windspeed_10m,dk.date_key,s.run_id FROM stg_weather_hourly s 
+SELECT s.location_id,s.ts,s.temperature_2m,s.precipitation,s.windspeed_10m,dk.date_key,s.run_id 
+FROM stg_weather_hourly s 
 JOIN dim_date dk ON dk.date=s.ts::date ON CONFLICT (location_id, ts) 
 DO UPDATE SET temperature_2m=EXCLUDED.temperature_2m,
 precipitation=EXCLUDED.precipitation,windspeed_10m=EXCLUDED.windspeed_10m,
